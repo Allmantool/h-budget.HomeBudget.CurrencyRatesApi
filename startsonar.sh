c3bed21bf09d7faa5696ea88b0a66a3e8ab3a27f
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2023-11-14 23:39:50 +0300

    fix: adjust path for sonar test coverage report

--- startsonar.sh
+++ startsonar.sh
@@ -7,14 +7,14 @@ if [ ${PULL_REQUEST_ID} ];
         /k:"Allmantool_h-budget-currency-rates" \
         /n:"h-budget-currency-rates" \
         /v:"${GITHUB_RUN_ID}" \
-        /d:sonar.login="${SONAR_TOKEN}" \
+        /d:sonar.token="${SONAR_TOKEN}" \
         /d:sonar.host.url="https://sonarcloud.io" \
         /d:sonar.pullrequest.key="${PULL_REQUEST_ID}" \
         /d:sonar.pullrequest.branch="${PULL_REQUEST_SOURCE_BRANCH:11}" \
         /d:sonar.pullrequest.base="${PULL_REQUEST_TARGET_BRANCH:11}" \
         /d:sonar.coverage.exclusions="**/Test[s]/**/*" \
         /d:sonar.coverageReportPaths="/app/testresults/coverage/reports/SonarQube.xml" \
-        /d:sonar.cs.opencover.reportsPaths="/app/testresults/coverage/currencyRates.coverage.xml" \
+        /d:sonar.cs.opencover.reportsPaths="testresults/coverage/currencyRates.coverage.xml" \
         /d:sonar.pullrequest.provider="github" \
         /d:sonar.pullrequest.github.repository="Allmantool/h-budget.HomeBudget.CurrencyRatesApi" \
         /d:sonar.pullrequest.github.endpoint="https://api.github.com/"; \
@@ -30,9 +30,9 @@ if [ ${PULL_REQUEST_ID} ];
         /n:"h-budget-currency-rates" \
         /v:"${GITHUB_RUN_ID}" \
         /d:sonar.branch.name="master" \
-        /d:sonar.login="${SONAR_TOKEN}" \
+        /d:sonar.token="${SONAR_TOKEN}" \
         /d:sonar.host.url="https://sonarcloud.io" \
         /d:sonar.coverageReportPaths="/app/testresults/coverage/reports/SonarQube.xml" \
-        /d:sonar.cs.opencover.reportsPaths="/app/testresults/coverage/currencyRates.coverage.xml" \
+        /d:sonar.cs.opencover.reportsPaths="testresults/coverage/currencyRates.coverage.xml" \
         /d:sonar.coverage.exclusions="**/Test[s]/**/*"; \
     fi;
