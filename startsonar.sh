60ab8288ed340740e1ec8c7f2dd13e56ce963cae
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2023-11-15 23:34:37 +0300

    chore: typo

--- startsonar.sh
+++ startsonar.sh
@@ -13,7 +13,7 @@ if [ ${PULL_REQUEST_ID} ];
             /d:sonar.pullrequest.branch="${PULL_REQUEST_SOURCE_BRANCH:11}" \
             /d:sonar.pullrequest.base="${PULL_REQUEST_TARGET_BRANCH}" \
             /d:sonar.coverage.exclusions="**/Test[s]/**/*" \
-            /d:sonar.testExecutionReportPaths="/src" \
+            /d:sonar.testExecutionReportPaths="test-results/SonarQube.xml" \
             /d:sonar.coverageReportPaths="test-results/SonarQube.xml" \
             /d:sonar.cs.opencover.reportsPaths="test-results/rates-coverage.xml" \
             /d:sonar.pullrequest.provider="github" \
@@ -33,7 +33,7 @@ if [ ${PULL_REQUEST_ID} ];
             /d:sonar.branch.name="master" \
             /d:sonar.token="${SONAR_TOKEN}" \
             /d:sonar.host.url="https://sonarcloud.io" \
-            /d:sonar.testExecutionReportPaths="/src" \
+            /d:sonar.testExecutionReportPaths="test-results/SonarQube.xml" \
             /d:sonar.coverageReportPaths="test-results/SonarQube.xml" \
             /d:sonar.cs.opencover.reportsPaths="test-results/rates-coverage.xml" \
             /d:sonar.coverage.exclusions="Test[s]/**/*"; \
