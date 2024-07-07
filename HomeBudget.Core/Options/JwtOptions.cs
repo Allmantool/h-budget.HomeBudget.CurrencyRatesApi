c94606cda8d7b6d417202ea881c8efd55e36bf87
ROOT-LAPTOP\pavel <pavel.chyzhykau@gmail.com>
2024-07-07 09:37:16 +0300

    tech(M7): add jwt base infrastructure

---
+++ HomeBudget.Core/Options/JwtOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeBudget.Core.Options
+{
+    public record JwtOptions
+    {
+        public string Secret { get; init; }
+    }
+}
---
+++ HomeBudget.Rates.Api/Extensions/JwtExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+using HomeBudget.Core.Options;
+using HomeBudget.Rates.Api.Middlewares;
+
+namespace HomeBudget.Rates.Api.Extensions
+{
+    internal static class JwtExtensions
+    {
+        public static void SetUpSecurity(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<JwtMiddleware>(); // JWT Middleware
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+
+        public static void AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var section = configuration.GetSection("jwt");
+            var options = section.Get<JwtOptions>();
+            var key = Encoding.UTF8.GetBytes(options.Secret);
+            section.Bind(options);
+            services.Configure<JwtOptions>(section);
+
+            // services.AddSingleton<IJwtBuilder, JwtBuilder>();
+            services.AddTransient<JwtMiddleware>();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization(x =>
+            {
+                x.DefaultPolicy = new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+        }
+    }
+}
